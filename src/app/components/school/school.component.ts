5160b817550ba9daeb5dcd3a2856a5bbf0e8a034
Wendell Simões <wendeo_o@hotmail.com>
2023-02-03 13:42:41 -0300

    finish school component

--- src/app/components/school/school.component.ts
+++ src/app/components/school/school.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-school',
   templateUrl: './school.component.html',
   styleUrls: ['./school.component.scss']
 })
-export class SchoolComponent {
+export class SchoolComponent implements OnInit {
+
+  public show: boolean = false;
+  
+  public showContent(): void {
+    setInterval(() => {
+      this.show = true;
+    }, 100);
+  }
+
+  ngOnInit(): void {
+    this.showContent();
+  }
 
 }
